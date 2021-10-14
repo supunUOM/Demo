702eff9433a7c68771864655c30aab2276f260df
supun <supunsandaruwan000000@gmail.com>
2021-10-14 23:46:12 +0530

    normal changes

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Demo
 
-this is readme file
 the new lien 
+this is readme file
+
+
+the new lien 
+this is readme file
+
+
+the new lien 
+this is readme file
--- app.js
+++ app.js
@@ -2,3 +2,10 @@ console.log("Hello world.");
 console.log("change by dev 2");
 console.log("testing 05 branch")
 
+console.log("change by dev 2");
+console.log("testing 05 branch")
+
+console.log("change by dev 2");
+console.log("testing 05 branch")
+console.log("testing 05 branch")
+
