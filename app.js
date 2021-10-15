fe44c442150b951018298c8b1f80a8f803354cca
supun <supunsandaruwan000000@gmail.com>
2021-10-15 21:30:02 +0530

    deleted items

--- app.js
+++ app.js
@@ -4,16 +4,3 @@ console.log("another line")
 console.log("change by dev 2");
 console.log("testing 05 branch")
 
-console.log("change by dev 2");
-console.log("testing 05 branch")
-
-console.log("change by dev 2");
-console.log("testing 05 branch")
-console.log("testing 05 branch")
-console.log("change by dev 2");
-console.log("testing 05 branch")
-console.log("testing 05 branch")
-console.log("change by dev 2");
-console.log("testing 05 branch")
-console.log("testing 05 branch")
-
